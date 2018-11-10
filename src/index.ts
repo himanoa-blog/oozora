027b758d8115fb5d0511e8ab225157333efcaf20
himanoa <matsunoappy@gmail.com>
2018-11-10 21:03:31 +0900

    Add dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
 import express from "express";
+import csrf from "csurf";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 
 const app = express()
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+app.use(bodyParser.json())
+app.use(cookieParser())
+app.use(csrf({ cookie: true }))
 
 app.get('/', (_, res) => {
   return res.send("hello")
