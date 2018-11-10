d941c6d6c19d8acb53308cd3fec228077cc05525
himanoa <matsunoappy@gmail.com>
2018-11-10 22:17:57 +0900

    feat: Enable log

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,23 @@ import csrf from "csurf";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import applyRouter from "./route";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import fs from "fs";
+import path from "path";
+
+dotenv.config()
 
 const app = express();
+if(app.get('env') === 'production') {
+  const logDirectory = path.resolve(process.env.LOG_DIR || "./log")
+  if(!fs.existsSync(logDirectory)) fs.mkdirSync(logDirectory)
+  const stream = fs.createWriteStream(path.resolve(logDirectory, "access.log"), { flags: 'a' });
+  app.use(morgan("common", { stream: stream }));
+} else {
+  app.use(morgan("common"))
+}
+
 app.use(bodyParser.urlencoded({
   extended: true
 }))
