bd6c064d99be403ad27050dfaa9257e613655d37
himanoa <matsunoappy@gmail.com>
2018-11-13 03:05:06 +0900

    feat: Implement login service

--- src/service/login.test.ts
+++ src/service/login.test.ts
@@ -1,5 +1,7 @@
 import { verifyToken, TokenPayload } from "./login";
 import { Certs } from "../ext/oauth";
+import { UserRepository } from "../repository/user-repository";
+import * as Login from "./login";
 
 test("tokenVerify#期待したトークンが返ってくること", async () => {
   const req = { state: "dummy", code: "dummy" };
@@ -21,7 +23,7 @@ test("tokenVerify#期待したトークンが返ってくること", async () =>
     ]
   };
   const dep = {
-    getToken: (code: string): Promise<string> =>
+    getToken: (_code: string): Promise<string> =>
       Promise.resolve(`${dummyEnvelope}.a.a`),
     getCerts: (): Promise<Certs> => Promise.resolve(dummyCerts),
     decoder: (_token: string, _n: string, _alg: string): any => ({ sub: "a" })
@@ -87,3 +89,77 @@ test("tokenVerify#envelopeにkidがない場合は例外を投げること", asy
   const actual = verifyToken(req, dep);
   await expect(actual).rejects.toThrow();
 });
+
+describe("login", () => {
+  const req = { state: "dummy", code: "dummy" };
+  const dummyEnvelope = Buffer.from(JSON.stringify({kid: "d"})).toString("base64");
+  const dummyCerts = {
+    keys: [
+      {
+        alg: "a",
+        n: "b",
+        use: "c",
+        kid: "d",
+        e: "e",
+        kty: "f"
+      }
+    ]
+  };
+  const vdep: Login.VerifyTokenDep = {
+    getToken: (_code: string): Promise<string> =>
+      Promise.resolve(`${dummyEnvelope}.a.a`),
+    getCerts: (): Promise<Certs> => Promise.resolve(dummyCerts),
+    decoder: (_token: string, _n: string, _alg: string): TokenPayload => ({
+      sub: "a"
+    })
+  }
+  const dummyRepository: UserRepository = {
+    resolve: (id: number) => Promise.resolve({
+      id,
+      name: "himanoa",
+      uid: "a",
+      updatedAt: new Date(),
+      createdAt: new Date()
+    }),
+    fromUid: (uid: string) => Promise.resolve({
+      id: 1,
+      name: "himanoa",
+      uid,
+      updatedAt: new Date(),
+      createdAt: new Date()
+    }),
+    fromToken: (_token: string) => Promise.resolve({
+      id: 1,
+      name: "himanoa",
+      uid: "a",
+      updatedAt: new Date(),
+      createdAt: new Date()
+    }),
+    createToken:(_u, _t) => Promise.resolve(1)
+  }
+  const dep: Login.LoginDep = {
+    verifyTokenDep: vdep,
+    generateToken: () => "a",
+    userRepository: dummyRepository
+  }
+
+  test("ログインユーザーが返ってくること", async () => {
+    const actual = await Login.login(req, dep)
+    expect(actual).toStrictEqual({
+      id: 1,
+      name: "himanoa",
+      token: "a"
+    })
+  })
+
+  test("ユーザーIDが存在しない場合、例外を投げること", async () => {
+    const repo = {
+      ...dummyRepository,
+      ...{
+        fromUid: (_id: string) => { throw new Login.LoginServiceException("gomi") }
+      }
+    };
+    const actual = Login.login(req, { ...dep, ...{ userRepository: repo } });
+    await expect(actual).rejects.toEqual(new Login.LoginServiceException('gomi'))
+  })
+})
--- src/service/login.ts
+++ src/service/login.ts
@@ -1,14 +1,34 @@
 import { LoginRequest } from "../model/login-request";
 import { LoginUser } from "../model/login-user";
 import { Cert, Certs } from "../ext/oauth";
+import { UserRepository } from "../repository/user-repository";
+
+export class LoginServiceException extends Error{
+  public name = 'LoginServiceException'
+
+  constructor(public message: string) {
+    super(message);
+  }
+
+  toStirng(){
+    return `${this.name}:${this.message}`
+  }
+}
 
 export interface TokenPayload {
   sub: string;
 }
+
 export interface VerifyTokenDep {
   getToken: (code: string) => Promise<string>;
   getCerts: () => Promise<Certs>;
   decoder: (token: string, n: string, alg: string) => TokenPayload;
+}
+
+export interface LoginDep {
+  verifyTokenDep: VerifyTokenDep;
+  generateToken: () => string;
+  userRepository: UserRepository;
 }
 
 export const decodeBase64 = (str: string): string =>
@@ -29,4 +49,23 @@ export async function verifyToken(
     (v: Cert) => v.kid === envelope.kid
   ) as Cert;
   return dep.decoder(idToken, key.n, key.alg);
+}
+
+export async function login(
+  req: LoginRequest,
+  dep: LoginDep,
+): Promise<LoginUser> {
+  try {
+    const { sub: uid } = await verifyToken(req, dep.verifyTokenDep);
+    const user = await dep.userRepository.fromUid(uid);
+    const token = dep.generateToken();
+    dep.userRepository.createToken(user, token)
+    return {
+      id: user.id,
+      token,
+      name: user.name
+    }
+  } catch (e) {
+    throw new LoginServiceException(e.message);
+  }
 }
